b1dd38553d38d887ed94168a3a79a9a77ceda66a
Алена Наддачина <miakaleekano@gmail.com>
2025-09-09 15:04:55 +0300

    Добавляет фильтрацию миниатюр

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,8 +1,19 @@
 import { showBigPicture } from './fullsize-picture.js';
+import { debounce } from './util.js';
+
+const RERENDER_DELAY = 500;
+const RANDOM_THUMBNAILS_COUNT = 10;
+
+const imgFilters = document.querySelector('.img-filters');
+const photosContainer = document.querySelector('.pictures');
+const btnFilterDefault = imgFilters.querySelector('#filter-default');
+const btnFilterRandom = imgFilters.querySelector('#filter-random');
+const btnFilterDiscussed = imgFilters.querySelector('#filter-discussed');
+
+let defaultThumbnails = [];
 
 const drawThumbnails = (items) => {
   const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
-  const photosContainer = document.querySelector('.pictures');
   const photosListFragment = document.createDocumentFragment();
 
   items.forEach(({url, description, likes, comments}) => {
@@ -19,6 +30,52 @@ const drawThumbnails = (items) => {
   });
 
   photosContainer.append(photosListFragment);
+
+  return items;
 };
 
-export { drawThumbnails };
+const clearThumbnails = () => {
+  photosContainer.querySelectorAll('.picture').forEach((el) => el.remove());
+};
+
+function showThumbnails(items) {
+  defaultThumbnails = items.slice();
+
+  drawThumbnails(defaultThumbnails);
+
+  imgFilters.classList.remove('img-filters--inactive');
+
+  btnFilterDefault.addEventListener('click', onBtnFilterDefault);
+  btnFilterRandom.addEventListener('click', onBtnFilterRandom);
+  btnFilterDiscussed.addEventListener('click', onBtnFilterDiscussed);
+}
+
+function changeFilter(filterBtn) {
+  const activeFilter = imgFilters.querySelector('.img-filters__button--active');
+  if (activeFilter) {
+    activeFilter.classList.remove('img-filters__button--active');
+  }
+  filterBtn.classList.add('img-filters__button--active');
+}
+
+function onBtnFilterDefault() {
+  clearThumbnails();
+  drawThumbnails(defaultThumbnails);
+  changeFilter(btnFilterDefault);
+}
+
+function onBtnFilterRandom() {
+  clearThumbnails();
+  const randomThumbnails = defaultThumbnails.slice().sort(() => 0.5 - Math.random()).slice(0, RANDOM_THUMBNAILS_COUNT);
+  drawThumbnails(randomThumbnails);
+  changeFilter(btnFilterRandom);
+}
+
+function onBtnFilterDiscussed() {
+  clearThumbnails();
+  const discussedThumbnails = defaultThumbnails.slice().sort((a, b) => b.comments.length - a.comments.length);
+  drawThumbnails(discussedThumbnails);
+  changeFilter(btnFilterDiscussed);
+}
+
+export { showThumbnails };
